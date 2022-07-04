2d4572de2ed6cd6f5034b120f15e736bbaaf91f7
Taylor <trferguson1701@gmail.com>
2022-07-04 14:46:32 -0700

    Update GuessingGame.py
    Added a functionality that compares players guess score to a computers guess score.

--- GuessingGame.py
+++ GuessingGame.py
@@ -2,6 +2,26 @@
 #Modules
 import random
 
+
+
+# Computer Guess Function
+def computerGuess (lowval, highval, randnum, count=0):
+    if highval >= lowval:
+        guess = lowval + (highval - lowval) // 2
+        if guess == randnum:
+            return count
+        #If the guess is greater than the number
+        elif guess > randnum:
+            count = count + 1
+            return computerGuess(lowval, guess-1, randnum, count)
+        else:
+            count = count + 1
+            return computerGuess(guess + 1, highval, randnum, count)
+    else:
+        #Number not found
+        return -1
+
+
 # Random Number Generator
 randnum = random.randint(1,101)
 
@@ -28,3 +48,4 @@ while (guess != randnum):
 # End loop
 
 print ("It took you " + str(count) + " tries to guess the number!")
+print ("It took the computer " + str(computerGuess(0,100, randnum)) + " tries to guess the number!")
